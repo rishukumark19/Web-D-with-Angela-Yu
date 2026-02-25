81952516dd9260b24f8281b39394ff7e4dfd934f
Rishu Kumar <96676931+rishukumark19@users.noreply.github.com>
2026-02-25 05:52:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const fname = "Rishu";
+const lname = "kumar";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>pizza</li>
+      <li>burger</li>
+      <li>pasta</li>
+      <li>{fname + " " + lname}</li>
+      <li>
+        {fname} {lname}
+      </li>
+      <li>{Math.floor(Math.random() * 10)}</li>
+    </ul>
+  </>
+);
